e06c3651fee43bbb4d181642e8c295d161de444b
PhilaniV <philanivezie@icloud.com>
2023-07-13 13:07:04 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - print a string to standard output
+ * Return: always return 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
