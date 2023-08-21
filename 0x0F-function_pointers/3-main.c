97d7c76908b6b0c8b5d09dcbc28da3610a4fa3f5
PhilaniV <philanivezie@icloud.com>
2023-08-21 14:42:08 -0700

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,17 +1,18 @@
 #include "3-calc.h"
-#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - ...
- * @argc: ...
- * @argv: ...
- *
- * Return: ...
+ * main - print program ops using argc and argv
+ * @argc: arg number in program
+ * @argv: arg vector in program
+ * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int (*oprt)(int, int);
+	int num1, num2;
+	char *op;
 
 	if (argc != 4)
 	{
@@ -19,14 +20,23 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	oprt = get_op_func(argv[2]);
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
 
-	if (!oprt)
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
+	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(op)(num1, num2));
+
 	return (0);
 }
