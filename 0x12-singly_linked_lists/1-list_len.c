445447d373c3a3d954c1d76b144b4fee5371fd3b
PhilaniV <philanivezie@icloud.com>
2023-08-24 15:59:22 -0700

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -6,9 +6,9 @@
  * Return: Integer.
  **/
 
-size_t list_len(const lst_t *h)
+size_t list_len(const list_t *h)
 {
-	const lst_t *temp;
+	const list_t *temp;
 	unsigned int counter = 0;
 
 	temp = h;
